c8bc0df99af516d55d92108f57136e0fd38924d4
weshare237 <fopaduclair2000@gmail.com>
2022-03-17 13:59:28 +0100

    An example of dfa that accepts set of all strings over {0,1} such tthat number of 1's are even

--- automaton/automata.hs
+++ automaton/automata.hs
@@ -6,3 +6,17 @@ data Automaton q s = Automaton {
                        finals   :: [q],
                        delta    :: q -> Maybe s -> [q]
                      }
+
+--An example of dfa that accepts set of all strings over {0, 1} where number of 1's are even
+nfa1 :: Automaton Char Int
+nfa1 = Automaton states alphabet initial finals delta
+       where states             = ['A', 'B']
+             alphabet           = [0, 1]
+             initial            = 'A'
+             finals             = ['A']
+             delta 'A' (Just 0) = ['A']
+             delta 'A' (Just 1) = ['B']
+             delta 'B' (Just 0) = ['B']
+             delta 'B' (Just 1) = ['A']
+           --delta 'A' Nothing  = ['B']
+             delta _   _        = [] 
